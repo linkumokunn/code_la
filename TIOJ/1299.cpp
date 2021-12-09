5349f43c9094dab63f0d611761def6222d48483e
linkumokunn <82722395+linkumokunn@users.noreply.github.com>
2021-12-09 23:31:18 +0800

    1209 update

---
+++ TIOJ/1299.cpp
@@ -0,0 +1,65 @@
+/*
+=============================================================================
+      /\       \      /      /\         |\           /|    |===========
+     / 	\       \    /      /  \        | \         / |    |
+    /    \       \  /      /    \       |  \       /  |    |
+   /      \       |       /      \      |   \     /   |    |===========
+  /--------\      |      /--------\     |    \   /    |    |
+ /          \     |     /          \    |     \ /     |    |
+/            \    |    /            \   |      |      |    |===========
+     A        A
+  AAAAAAAA   AAA AAAA      A    A      A     A        A          A
+    A    A    A A    A     A     A     A      A      A A        A A
+   A     A    AA      A    A      A    A       A    A   A      A   A
+  A   A  A   AA     A      A  A   A    A  A    A
+ A     AAA    A    A       AAA         AAA               AAAAA
+=============================================================================
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define F first
+#define S second
+#define pq priority_queue
+#define ll long long int
+#define pii pair<int,int>
+#define endl '\n'
+#define Orz ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAXN 10001
+using namespace std;
+//YeedragOrz
+//8e7Orz
+//jassOrz
+//FHvirusOrz
+//samOrz
+//yungyaoOrz
+//littlecubeOrz
+//2qbingxuanOrz
+//FoodSheepOrz
+//ck_platypusOrz
+//koyingOrz
+//foxyyOrz
+//peiganOrz
+//jikuaiOrz
+
+int main(){
+	string s;
+	while(cin>>s){
+		int cnt=0;
+		for(int i=0;i<s.size();i++){
+			if(s[i]<'a')s[i]+='a'-'A';cnt+=s[i]-'a'+1;
+		}
+		cnt%=17;
+		if(cnt<3){
+			cout<<"D\n";
+		}else if(cnt<6){
+			cout<<"C\n";
+		}else if(cnt<10){
+			cout<<"B\n";
+		}else if(cnt<13){
+			cout<<"A\n";
+		}else cout<<"SA\n";
+	}
+	return 0 ;
+} 
