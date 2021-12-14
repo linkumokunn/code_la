3d54b3b96d376ed647986782259b1ac073cecef5
linkumokunn <82722395+linkumokunn@users.noreply.github.com>
2021-12-14 23:10:02 +0800

    12/14 update

---
+++ TIOJ/1294.cpp
@@ -0,0 +1,53 @@
+/*
+=============================================================================
+      /\       \      /      /\         |\           /|    |===========
+     / 	\       \    /      /  \        | \         / |    |
+    /    \       \  /      /    \       |  \       /  |    |
+   /      \       |       /      \      |   \     /   |    |===========
+  /--------\      |      /--------\     |    \   /    |    |
+ /          \     |     /          \    |     \ /     |    |
+/            \    |    /            \   |      |      |    |===========
+     A        A
+  AAAAAAAA   AAA AAAA      A    A      A     A        A          A
+    A    A    A A    A     A     A     A      A      A A        A A
+   A     A    AA      A    A      A    A       A    A   A      A   A
+  A   A  A   AA     A      A  A   A    A  A    A
+ A     AAA    A    A       AAA         AAA               AAAAA
+=============================================================================
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define F first
+#define S second
+#define pq priority_queue
+#define ll long long int
+#define pii pair<int,int>
+#define endl '\n'
+#define Orz ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAXN 40001
+using namespace std;
+//YeedragOrz
+//8e7Orz
+//jassOrz
+//FHvirusOrz
+//samOrz
+//yungyaoOrz
+//littlecubeOrz
+//2qbingxuanOrz
+//FoodSheepOrz
+//ck_platypusOrz
+//koyingOrz
+//foxyyOrz
+//peiganOrz
+//jikuaiOrz
+
+int main(){
+	int arr[5]={0,1,9,83,7122},n;
+	while(cin>>n)cout<<arr[n]<<endl;
+	return 0;
+}
+
+
+
