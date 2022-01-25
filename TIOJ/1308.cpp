49fc85e6dc6f92ab04c6360c61d300564213c4f7
linkumokunn <82722395+linkumokunn@users.noreply.github.com>
2022-01-25 23:35:58 +0800

    01/25 update

---
+++ TIOJ/1308.cpp
@@ -0,0 +1,63 @@
+/*
+=============================================================================
+      /\       \      /      /\         |\           /|    |===========
+     / 	\       \    /      /  \        | \         / |    |
+    /    \       \  /      /    \       |  \       /  |    |
+   /      \       |       /      \      |   \     /   |    |===========
+  /--------\      |      /--------\     |    \   /    |    |
+ /          \     |     /          \    |     \ /     |    |
+/            \    |    /            \   |      |      |    |===========
+     A        A
+  AAAAAAAA   AAA AAAA      A    A      A     A        A          A
+    A    A    A A    A     A     A     A      A      A A        A A
+   A     A    AA      A    A      A    A       A    A   A      A   A
+  A   A  A   AA     A      A  A   A    A  A    A
+ A     AAA    A    A       AAA         AAA               AAAAA
+=============================================================================
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define F first
+#define S second
+#define pq priority_queue
+#define ll long long int
+#define pii pair<int,int>
+#define endl '\n'
+#define Orz ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAXN 100000
+#define MOD 1000000009
+using namespace std;
+//YeedragOrz
+//8e7Orz
+//jassOrz
+//FHvirusOrz
+//samOrz
+//yungyaoOrz
+//littlecubeOrz
+//2qbingxuanOrz
+//FoodSheepOrz
+//ck_platypusOrz
+//koyingOrz
+//foxyyOrz
+//peiganOrz
+//jikuaiOrz
+ll dp[51][51]={};
+ll QAQ(int n,int m){
+	if(dp[n][m]!=0)return dp[n][m];
+	if(n==m||m==0)return 1;
+	dp[n][m]=QAQ(n-1,m-1)+QAQ(n-1,m);
+	return dp[n][m];
+}
+int main(){
+	int n,m;cin>>n>>m;
+	while(n!=0){
+		cout<<QAQ(n+m-1,m)<<endl;
+		cin>>n>>m;
+	}
+	return 0;
+}
+
+
+
