2800427d7565c995eaaea36fc21321e98cd2a04d
linkumokunn <82722395+linkumokunn@users.noreply.github.com>
2022-04-06 14:52:38 +0800

    4/6 update

---
+++ NEOJ/74.cpp
@@ -0,0 +1,72 @@
+/*
+=============================================================================
+      /\       \      /      /\         |\           /|    |===========
+     / 	\       \    /      /  \        | \         / |    |
+    /    \       \  /      /    \       |  \       /  |    |
+   /      \       |       /      \      |   \     /   |    |===========
+  /--------\      |      /--------\     |    \   /    |    |
+ /          \     |     /          \    |     \ /     |    |
+/            \    |    /            \   |      |      |    |===========
+     A        A
+  AAAAAAAA   AAA AAAA      A    A      A     A        A          A
+    A    A    A A    A     A     A     A      A      A A        A A
+   A     A    AA      A    A      A    A       A    A   A      A   A
+  A   A  A   AA     A      A  A   A    A  A    A
+ A     AAA    A    A       AAA         AAA               AAAAA
+=============================================================================
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define F first
+#define S second
+#define pq priority_queue
+#define ll long long int
+#define pii pair<int,int>
+#define endl '\n'
+#define Orz ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAXN 10001
+#define BIG 998244353
+using namespace std;
+//YeedragOrz
+//8e7Orz
+//jassOrz
+//FHvirusOrz
+//samOrz
+//yungyaoOrz
+//littlecubeOrz
+//2qbingxuanOrz
+//FoodSheepOrz
+//ck_platypusOrz
+//koyingOrz
+//foxyyOrz
+//peiganOrz
+//jikuaiOrz
+
+int main(){	
+	Orz
+	int t;cin>>t;
+	while(t--){
+		int n,tmp,tai=-1,con=1,ans=1;
+		cin>>n;
+		while(n--){
+			cin>>tmp;
+			if(con){
+				if(tmp>tai){
+					tai=tmp;
+				}else if(tmp<tai)ans++,tai=tmp,con^=1;
+			}else{
+				if(tmp<tai){
+					tai=tmp;
+				}else if(tmp>tai)ans++,tai=tmp,con^=1;
+			}
+		}
+		if(con==0)cout<<ans-1<<endl;
+		else cout<<ans<<endl;
+	}
+	return 0;
+}
+
+
+
