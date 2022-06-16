b762bf9f1bf9fa5e7e18d45d530f103ac0e4e9c0
linkumokunn <82722395+linkumokunn@users.noreply.github.com>
2022-06-16 22:04:07 +0800

    6/16 update

--- 資訊專題/量杯問題.cpp
+++ 資訊專題/量杯問題.cpp
@@ -26,9 +26,11 @@
 #define pii pair<int,int>
 #define endl '\n'
 #define Orz ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define MOD 1000000007
-#define cos 257
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/hash_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
 //YeedragOrz
 //8e7Orz
 //jassOrz
@@ -44,24 +46,32 @@ using namespace std;
 //peiganOrz
 //jikuaiOrz
 int n,arr[5]={},t,ans=-1,cnt=0;//n:量杯數量 arr:量杯容量 t:要求容量 ans:答案 cnt:目前步驟數 
-set<ll> se;//存hash值 
-queue<vector<int>> q;//bfs用 queue 
-bool check(vector<int> a){//檢查hash值 
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    int operator()(int x) const { return x ^ RANDOM; }
+};
+gp_hash_table<int,bool> ma;//存hash值 
+queue<vector<int>,list<vector<int>>> q;//bfs用 queue 
+const ll MOD=1e9+7;
+const ll mul=1117;
+inline bool check(vector<int> a){//檢查hash值 
 	ll val=0;
-	for(int i=0;i<n;i++)val=(val*cos)%MOD,val+=a[i];
-	if(se.find(val)!=se.end())return 0;
+	for(int i=0;i<n;i++)val+=a[i],val=(val*mul)%MOD;
+	if(ma[val])return 0;
 	else return 1;
 }
-void add(vector<int> a){//新增hash值 
+inline void add(vector<int> a){//新增hash值 
 	ll val=0;
-	for(int i=0;i<n;i++)val=(val*cos)%MOD,val+=a[i];
-	se.insert(val);
+	for(int i=0;i<n;i++)val+=a[i],val=(val*mul)%MOD;
+	ma[val]=1;
 	return ;
 }
 int main(){
+	clock_t st,no;
 	cin>>n;
 	for(int i=0;i<n;i++)cin>>arr[i];
 	cin>>t;//輸入 
+	st=clock();
 	int g=arr[0],big=1;//g:存gcd big:紀錄有無比要求容量小的量杯 
 	for(int i=0;i<n;i++){
 		if(arr[i]==t){//判掉一步解方便後續程式設計 
@@ -75,14 +85,17 @@ int main(){
 		cout<<-1<<endl;
 		return 0;
 	}
-	vector<int> sta,tmp;for(int i=0;i<n;i++)sta.pb(0);//初始狀態 
+	vector<int> sta,tmp;
+	sta.reserve(5);tmp.reserve(5);
+	for(int i=0;i<n;i++)sta.pb(0);//初始狀態 
 	q.push(sta);
 	while(q.size()){//bfs
 		cnt++;
+		no=clock();
+		cout<<cnt<<" "<<double(no-st)/CLOCKS_PER_SEC<<"秒"<<endl;
 		int siz=q.size();
 		while(siz--){//同一步驟數一起跑 
 			sta=q.front();q.pop();
-			//for(int i=0;i<n;i++)cout<<" "<<sta[i];cout<<endl;
 			for(int i=0;i<n;i++){
 				if(sta[i]!=arr[i]){//倒滿 
 					tmp=sta;tmp[i]=arr[i];
@@ -97,9 +110,9 @@ int main(){
 			}
 			for(int i=0;i<n;i++){
 				for(int j=0;j<n;j++){
-					if(i!=j){//i倒到j 
+					if(i!=j)if(sta[i]!=0&&(arr[j]-sta[j])){//i倒到j 
 						tmp=sta;
-						int ttmp=min(tmp[i],arr[j]-tmp[j]);
+						ll ttmp=min(tmp[i],arr[j]-tmp[j]);
 						tmp[j]+=ttmp;
 						tmp[i]-=ttmp;
 						if(tmp[i]==t||tmp[j]==t)ans=cnt;//檢查答案 
@@ -110,6 +123,7 @@ int main(){
 		}
 		if(ans!=-1)break;
 	}
-	cout<<ans<<endl;
+	no=clock();
+	cout<<ans<<" "<<double(no-st)/CLOCKS_PER_SEC<<"秒"<<endl;
 	return 0;
 }
