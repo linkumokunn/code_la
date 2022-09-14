ded516e6ce399e85179704598258e46bc313fb84
linkumokunn <82722395+linkumokunn@users.noreply.github.com>
2022-09-14 22:25:48 +0800

    2022/9/14 update

---
+++ atcoder/arc137C.cpp
@@ -0,0 +1,62 @@
+/*
+=============================================================================
+      /\       \      /      /\         |\           /|    |===========
+     / 	\       \    /      /  \        | \         / |    |
+    /    \       \  /      /    \       |  \       /  |    |
+   /      \       |       /      \      |   \     /   |    |===========
+  /--------\      |      /--------\     |    \   /    |    |
+ /          \     |     /          \    |     \ /     |    |
+/            \    |    /            \   |      |      |    |===========
+     A        A
+  AAAAAAAA   AAA AAAA      A    A      A     A        A          A
+    A    A    A A    A     A     A     A      A      A A        A A
+   A     A    AA      A    A      A    A       A    A   A      A   A
+  A   A  A   AA     A      A  A   A    A  A    A
+ A     AAA    A    A       AAA         AAA               AAAAA
+=============================================================================
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define F first
+#define S second
+#define pq priority_queue
+#define ll long long int
+#define pii pair<int,int>
+#define endl '\n'
+#define Orz ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAXN 100001
+using namespace std;
+//YeedragOrz
+//8e7Orz
+//jassOrz
+//FHvirusOrz
+//samOrz
+//yungyaoOrz
+//littlecubeOrz
+//2qbingxuanOrz
+//FoodSheepOrz
+//ck_platypusOrz
+//koyingOrz
+//foxyyOrz
+//peiganOrz
+//jikuaiOrz
+
+
+int main(){
+	int n;cin>>n;
+	int arr[n],ma=-1e9;for(int i=0;i<n;i++)cin>>arr[i],ma=max(ma,arr[i]);
+	int flag=0;
+	if(arr[n-2]+1<arr[n-1])flag=1;
+	else flag=(arr[n-1]-(n-1))%2;
+	if(flag){
+		cout<<"Alice\n";
+	}else{
+		cout<<"Bob\n";
+	}
+}
+
+
+
+
